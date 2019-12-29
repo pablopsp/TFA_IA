d09623b15397e144ef5c460a1bf0e49648b1cf73
unknown <pablosilio@gmail.com>
2019-12-30 00:04:41 +0100

    Frequencies

--- NLP.py
+++ NLP.py
@@ -1,6 +1,18 @@
 import nltk
 import pandas as pd
+import re
+
 
 noticias = pd.read_excel('data/Ibex35Noticias.xlsx', sheet_name=None)
-[companie.drop(['Unnamed: 0'], axis='columns', inplace=True) for companie in data.values()]
+[companie.drop(['Unnamed: 0'], axis='columns', inplace=True) for companie in noticias.values()]
+
+for key, compData in noticias.items():
+    freqs = []
+    for x in compData['Texto']:
+        pattern = re.compile(r'\W+')
+        tokens = pattern.split(x)
+        freq = nltk.FreqDist(tokens)
+        freqs.append(pd.DataFrame(list(freq.items()), columns = ["Word","Frequency"]))
+        
+    compData['Frequencies'] = freqs
 
